07bd30529aa46ad5b3c7cbd7b801c93368b2b04f
volgoweb <volgoweb@bk.ru>
2013-09-24 17:44:33 +0400

    Добавил функционал добавления сущности в список и поправил автодополняемое поле для добавления сущности

--- tpl/lists_collection_page.tpl.php
+++ tpl/lists_collection_page.tpl.php
@@ -1,7 +1,7 @@
 <table id="rel-lists">
 	<tr>
 		<?php for ($i = 0; $i < count($lists); $i++): ?>
-			<td><a href="/<?php print REL_URL_ADMIN . '/list/' . $lists[$i]->id; ?>"><?php print $lists[$i]->title; ?></a></td>
+			<td><a href="/<?php print sprintf('%s/list/%d/params', REL_URL_ADMIN, $lists[$i]->id); ?>"><?php print $lists[$i]->title; ?></a></td>
 		<?php endfor; ?>
 	</tr>
 </table>
