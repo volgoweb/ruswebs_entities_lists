8a3a66a1d2a5faed1982508be7f716b23bed6c43
volgoweb <volgoweb@bk.ru>
2013-09-25 15:57:17 +0400

    частично сделал сортировку по весу сущности в списке

--- tpl/lists_collection_page.tpl.php
+++ tpl/lists_collection_page.tpl.php
@@ -1,8 +1,8 @@
 <table id="rel-lists">
-	<tr>
-		<?php for ($i = 0; $i < count($lists); $i++): ?>
-			<td><a href="/<?php print sprintf('%s/list/%d', REL_URL_ADMIN_LISTS, $lists[$i]->id); ?>"><?php print $lists[$i]->title; ?></a></td>
-		<?php endfor; ?>
-	</tr>
+	<?php for ($i = 0; $i < count($lists); $i++): ?>
+		<tr>
+				<td><a href="/<?php print sprintf('%s/list/%d', REL_URL_ADMIN_LISTS, $lists[$i]->id); ?>"><?php print $lists[$i]->title; ?></a></td>
+		</tr>
+	<?php endfor; ?>
 </table>
 <a class="button add-list" href="/<?php print REL_URL_ADMIN_ADD_LIST; ?>">Добавить список</a>
